15b23d99388d7f787b9a7048ad24f873a231d299
Hal Hyatt <feewet@gmail.com>
2020-10-12 13:55:52 -0700

    pool token

--- contracts/PoolToken.sol
+++ contracts/PoolToken.sol
@@ -11,11 +11,11 @@ interface IDistributor {
 interface IFarm {
 }
 
-interface IPoolToken is IERC20 {
-    function mint(address account, uint256 amount) external;
-    function redeem(uint256 amount) external;
+interface IPoolToken {
+    function deposit(address account, uint256 amount) external;
     function withdraw(uint256 amount) external;
-    function reward(address account) external view returns (uint256);
+    function claim() external;
+    function unclaimed(address account) external view returns (uint256);
 }
 
 /**
@@ -23,9 +23,14 @@ interface IPoolToken is IERC20 {
  */
 contract PoolToken is IPoolToken, ERC20, Claimable {
     using SafeMath for uint256;
-    mapping (address => uint256) stakes;
     IERC20 stakeToken;
     IERC20 rewardToken;
+    // cumulative rewards per stake
+    uint256 cumulative;
+    // claimed cumulative rewards for each account
+    mapping (address => uint256) claimed;
+    // default ratio of deposit to stake amount
+    uint256 constant DEFAULT_RATIO = 1000;
 
     constructor(IERC20 _stakeToken, IERC20 _rewardToken) public {
         stakeToken = _stakeToken;
@@ -34,11 +39,11 @@ contract PoolToken is IPoolToken, ERC20, Claimable {
     }
 
     function name() public view override returns (string memory) {
-        return "DAI";
+        return "Artist Pool";
     }
 
     function symbol() public view override returns (string memory) {
-        return "DAI";
+        return "ARTIST-POOL";
     }
 
     function decimals() public view override returns (uint8) {
@@ -46,30 +51,82 @@ contract PoolToken is IPoolToken, ERC20, Claimable {
     }
 
     /**
-     * @dev deposit stake tokens & mint pool tokens
+     * @dev mint and calculate new claimed cumulative rewards
      */
-    function mint(address account, uint256 amount) external override {
+    function mint(address account, uint256 amount) internal {
+        _mint(account, amount);
+        uint256 prior = balanceOf(account);
+
+        // ((cumulative * amount) - (claimed * prior)) / (prior + amount)
+        claimed[msg.sender] = cumulative.mul(amount)
+            .sub(claimed[account].mul(prior))
+            .div(prior.sub(amount));
+    }
+
+    function burn(address account, uint256 amount) internal {
+        _burn(account, amount);
+        // TODO
+    }
+
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        return super.transfer(recipient, amount);
+        // TODO
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
+        return super.transferFrom(sender, recipient, amount);
         // TODO
     }
 
     /**
-     * @dev burn pool tokens for stake tokens
+     * @dev deposit stake tokens & mint pool tokens
      */
-    function redeem(uint256 amount) external override {
-        // TODO
+    function deposit(address account, uint256 amount) external override {
+        require(stakeToken.transferFrom(account, address(this), amount));
+        // get balance of stake tokens
+        uint256 balance = stakeToken.balanceOf(address(this));
+        // get total stake
+        uint256 totalStake = totalSupply();
+
+        // calcualte stake for deposit amount
+        uint256 stake;
+        if (amount < balance) {
+            // stake = (amount * total) / (balance - amount)
+            stake = amount.mul(totalStake).div(balance.sub(amount));
+        }
+        // first staker
+        else {
+            require(totalStake == 0, "pool drained");
+            stake = amount.mul(DEFAULT_RATIO);
+        }
+        // mint stake for account
+        mint(account, stake);
     }
 
     /**
      * @dev withdraw reward tokens
      */
     function withdraw(uint256 amount) external override {
-        // TODO
+        uint256 unstake = balanceOf(msg.sender);
+
+    }
+
+    function claim() external override {
+        uint256 stake = balanceOf(msg.sender);
+        if (stake == 0) {
+            return;
+        }
+
+        uint256 reward = unclaimed(msg.sender);
+        claimed[msg.sender] = cumulative;
+        require(rewardToken.transfer(msg.sender, reward), "pool out of reward");
     }
 
     /**
      * @dev calculate reward amount for account
      */
-    function reward(address account) public view override returns (uint256) {
-        // TODO
+    function unclaimed(address account) public view override returns (uint256) {
+        // unclaimed rewards = stake * (cumulative - claimed)
+        return balanceOf(account).mul(cumulative.sub(claimed[account]));
     }
 }
